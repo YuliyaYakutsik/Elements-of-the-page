dc1371152311ea7a3bca780f7b5cd44551516432
Yuliya Yakutsik <juliayakutik@gmail.com>
2017-02-16 23:20:58 +0200

    Tabs done

---
+++ TABS/tabs.js
@@ -0,0 +1,37 @@
+//tabs
+$(document).ready(function(){
+	$('.tabs__controls-link').on('click', function(e) {
+  		e.preventDefault();
+   		var item=$(this).closest('.tabs__controls-item');
+   		var contentItem=$('.tabs__item');
+   		var position=item.index();
+   		contentItem.eq(position)
+   			.addClass('tabs__item_active')
+   			.siblings()
+   			.removeClass('tabs__item_active')
+   		item
+   			.addClass('tabs__controls-item_active')
+   			.siblings()
+   			.removeClass('tabs__controls-item_active')
+  	});
+});//ready
+
+
+
+//tabs with no worry about the order in the list
+$(document).ready(function(){
+	$('.tabs__controls1-link').on('click', function(e) {
+  		e.preventDefault();
+   		var item1=$(this).closest('.tabs__controls1-item');
+   		var contentItem1=$('.tabs__item1');
+   		var position1=item1.data('class');
+   		contentItem1.filter('.tabs__item1_'+ position1)
+   			.addClass('tabs__item1_active')
+   			.siblings()
+   			.removeClass('tabs__item1_active')
+   		item1
+   			.addClass('tabs__controls1-item_active')
+   			.siblings()
+   			.removeClass('tabs__controls1-item_active')
+  	});
+});//ready
